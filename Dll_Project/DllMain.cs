b3b7c632bdd4b0ef6e3827e3203ebc4ebb3c603a
wz <iamwanzan@qq.com>
2020-09-02 09:30:29 +0800

    dll project

---
+++ Dll_Project/DllMain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Dll_Project
+{
+    public static class DllMain
+    {
+        public static void Main()
+        {
+            UnityEngine.Debug.Log("Dll Run Main !");
+
+            var extralData = DllManager.Instance.transform.GetComponent<HFExtralData>();
+
+            if (extralData != null && extralData.ExtralDatas.Length > 0)
+            {
+                extralData.ExtralDatas[0].Target.gameObject.SetActive(true);
+            }
+        }
+    }
+}
---
+++ Dll_Project/HotfixTestMono.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dll_Project
+{
+    public class TestNewClass
+    {
+        public string showInfo = "showInfo: HoffixTestMono Init !";
+        public string showInfo1 = "showInfo1: HoffixTestMono Awake !";
+    }
+
+    public class HoffixTestMono : DllGenerateBase
+    {
+        TestNewClass testNewClass = new TestNewClass();
+
+        public override void Init()
+        {
+            base.Init();
+
+            Debug.Log(testNewClass.showInfo);
+        }
+        public override void Awake()
+        {
+            base.Awake();
+
+            if(BaseMono.ExtralDatas.Length > 0)
+            {
+                BaseMono.ExtralDatas[0].Target.gameObject.SetActive(false);
+            }
+
+            Debug.Log(testNewClass.showInfo1);
+        }
+
+        public override void Start()
+        {
+            base.Start();
+
+            Debug.Log("HoffixTestMono Start !");
+        }
+
+        public override void OnEnable()
+        {
+            base.OnEnable();
+
+            Debug.Log("HoffixTestMono OnEnable !");
+        }
+
+        public override void OnDisable()
+        {
+            base.OnDisable();
+
+            Debug.Log("HoffixTestMono OnDisable !");
+        }
+    }
+}
