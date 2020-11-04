8a15187140beb902f7f26e7f55f824dbda5a9016
wz <wz@vswork.com>
2020-11-04 15:02:04 +0800

    update

--- Dll_Project/DllMain.cs
+++ Dll_Project/DllMain.cs
@@ -1,18 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace Dll_Project
 {
     public static class DllMain
     {
+        public static List<GameObject> ClickObjs = new List<GameObject>();
+        public static List<GameObject> ShowObjs = new List<GameObject>();
         public static void Main()
         {
             UnityEngine.Debug.Log("Dll Run Main !");
 
+            ClickObjs.Clear();
+            ShowObjs.Clear();
+
+            foreach (var obj in DllManager.Instance.ExtralDatas)
+            {
+                if (obj.Target != null)
+                {
+                    if (obj.OtherData.Equals("0"))
+                    {
+                        ClickObjs.Add(obj.Target.gameObject);
+                    }
+                    else if(obj.OtherData.Equals("1"))
+                    {
+                        ShowObjs.Add(obj.Target.gameObject);
+                    }
+                }
+            }
+
             var extralData = DllManager.Instance.transform.GetComponent<HFExtralData>();
 
-            if (extralData != null && extralData.ExtralDatas.Length > 0)
+            foreach (var obj in extralData.ExtralDatas)
             {
-                extralData.ExtralDatas[0].Target.gameObject.SetActive(true);
+                if (obj.Target != null)
+                {
+                    obj.Target.gameObject.SetActive(true);
+                }
             }
 
             //com.ootii.Messages.MessageDispatcher.SendMessage(DllManager.Instance, "STARTED", "Whoo Hoo!", 0);
