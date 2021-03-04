e1313530169c9398d13a5abfd19faf1563da9f15
ben <qibentie@163.com>
2021-03-04 22:36:39 +0800

    add a namespace for demo

--- Dll_Project/Click_show_hideDemo.cs
+++ Dll_Project/Click_show_hideDemo.cs
@@ -5,185 +5,194 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Click_show_hideDemo : DllGenerateBase
+namespace Dll_Project
 {
-    public VRPointObjEventType PointEventType = VRPointObjEventType.VRPointClick;
-
-    public List<GameObject> ClickObjs = new List<GameObject>();
-    public List<GameObject> ShowObjs = new List<GameObject>();
-
-    public GameObject ClickedObj;
-    public GameObject showItem;
-    public GameObject Recieve_ShowItem;
-
-    private Tweener tween;
-
-    public string clickedName = string.Empty;
-    public string showItemName = string.Empty;
-
-    public string Recieve_A = string.Empty;
-    public string Recieve_B = string.Empty;
-
-
-    public override void Awake()
+    public class Click_show_hideDemo : DllGenerateBase
     {
-        base.Awake();
+        public VRPointObjEventType PointEventType = VRPointObjEventType.VRPointClick;
 
-        ClickObjs.Clear();
-        ShowObjs.Clear();
+        public List<GameObject> ClickObjs = new List<GameObject>();
+        public List<GameObject> ShowObjs = new List<GameObject>();
 
-        foreach (var obj in BaseMono.ExtralDatas)
+        public GameObject ClickedObj;
+        public GameObject showItem;
+        public GameObject Recieve_ShowItem;
+
+        private Tweener tween;
+
+        public string clickedName = string.Empty;
+        public string showItemName = string.Empty;
+
+        public string Recieve_A = string.Empty;
+        public string Recieve_B = string.Empty;
+
+        public override void Init()
         {
-            if (obj.Target != null)
+            base.Init();
+
+            Debug.Log("Click_show_hideDemo Init !");
+        }
+
+        public override void Awake()
+        {
+            base.Awake();
+            Debug.Log("Click_show_hideDemo Awake !");
+            ClickObjs.Clear();
+            ShowObjs.Clear();
+
+            foreach (var obj in BaseMono.ExtralDatas)
             {
-                if (obj.OtherData.Equals("0"))
+                if (obj.Target != null)
                 {
-                    ClickObjs.Add(obj.Target.gameObject);
-                }
-                else if (obj.OtherData.Equals("1"))
-                {
-                    ShowObjs.Add(obj.Target.gameObject);
+                    if (obj.OtherData.Equals("0"))
+                    {
+                        ClickObjs.Add(obj.Target.gameObject);
+                    }
+                    else if (obj.OtherData.Equals("1"))
+                    {
+                        ShowObjs.Add(obj.Target.gameObject);
+                    }
                 }
             }
         }
-    }
 
-    public override void OnEnable()
-    {
-        base.OnEnable();
-
-        MessageDispatcher.AddListener(PointEventType.ToString(), GetPointEventType);
-        MessageDispatcher.AddListener(WsMessageType.RecieveCChangeObj.ToString(), RecieveCChangeObj);
-    }
-
-    public override void OnDisable()
-    {
-        base.OnDisable();
-
-        MessageDispatcher.RemoveListener(PointEventType.ToString(), GetPointEventType);
-        MessageDispatcher.RemoveListener(WsMessageType.RecieveCChangeObj.ToString(), RecieveCChangeObj);
-    }
-
-    void GetPointEventType(IMessage msg)
-    {
-        GameObject pobj = msg.Data as GameObject;
-        ClickedObj = pobj;
-
-        HandleGetPointedObj();
-    }
-
-    void HandleGetPointedObj()
-    {
-        clickedName = ClickedObj.name + " has clicked !";
-
-        if (mStaticThings.I == null) { return; }
-        WsChangeInfo wsinfo = new WsChangeInfo()
+        public override void OnEnable()
         {
-            id = mStaticThings.I.mAvatarID,
-            name = "InfoLog",
-            a = clickedName,
-            b = InfoColor.green.ToString(),
-            c = 5.ToString(),
-        };
-
-        MessageDispatcher.SendMessage(this, VrDispMessageType.SendInfolog.ToString(), wsinfo, 0);
-
-        int _id = -1;
-
-        if (ClickedObj == null || ClickedObj.Equals(null))
-        {
-            _id = ClickObjs.FindIndex(x => x == null || x.Equals(null));
-        }
-        else
-        {
-            _id = ClickObjs.IndexOf(ClickedObj);
+            base.OnEnable();
+            Debug.Log("Click_show_hideDemo OnEnable !");
+            MessageDispatcher.AddListener(PointEventType.ToString(), GetPointEventType);
+            MessageDispatcher.AddListener(WsMessageType.RecieveCChangeObj.ToString(), RecieveCChangeObj);
         }
 
-        var _iscontained = _id != -1;
-
-        if (!_iscontained) return;
-
-        if (_id >= ShowObjs.Count) return;
-
-        showItem = ShowObjs[_id];
-
-        showItemName = showItem.name;
-
-        if (showItem.gameObject.activeInHierarchy)
+        public override void OnDisable()
         {
-            WsCChangeInfo wsinfo1 = new WsCChangeInfo()
+            base.OnDisable();
+
+            MessageDispatcher.RemoveListener(PointEventType.ToString(), GetPointEventType);
+            MessageDispatcher.RemoveListener(WsMessageType.RecieveCChangeObj.ToString(), RecieveCChangeObj);
+        }
+
+        void GetPointEventType(IMessage msg)
+        {
+            GameObject pobj = msg.Data as GameObject;
+            ClickedObj = pobj;
+
+            HandleGetPointedObj();
+        }
+
+        void HandleGetPointedObj()
+        {
+            clickedName = ClickedObj.name + "has clicked !";
+
+            if (mStaticThings.I == null) { return; }
+            WsChangeInfo wsinfo = new WsChangeInfo()
             {
-                a = "hideitem",
-                b = showItemName,
-                c = string.Empty,
-                d = string.Empty,
-                e = string.Empty,
-                f = string.Empty,
-                g = string.Empty,
+                id = mStaticThings.I.mAvatarID,
+                name = "InfoLog",
+                a = clickedName,
+                b = InfoColor.green.ToString(),
+                c = 5.ToString(),
             };
 
-            MessageDispatcher.SendMessage(this, WsMessageType.SendCChangeObj.ToString(), wsinfo1, 0);
-        }
-        else
-        {
-            WsCChangeInfo wsinfo1 = new WsCChangeInfo()
+            MessageDispatcher.SendMessage(this, VrDispMessageType.SendInfolog.ToString(), wsinfo, 0);
+
+            int _id = -1;
+
+            if (ClickedObj == null || ClickedObj.Equals(null))
             {
-                a = "showitem",
-                b = showItemName,
-                c = string.Empty,
-                d = string.Empty,
-                e = string.Empty,
-                f = string.Empty,
-                g = string.Empty,
-            };
-
-            MessageDispatcher.SendMessage(this, WsMessageType.SendCChangeObj.ToString(), wsinfo1, 0);
-        }
-    }
-
-
-    void RecieveCChangeObj(IMessage msg)
-    {
-        WsCChangeInfo rinfo = msg.Data as WsCChangeInfo;
-
-        Recieve_A = rinfo.a;
-        Recieve_B = rinfo.b;
-        //c.Value = rinfo.c;
-        //d.Value = rinfo.d;
-        //e.Value = rinfo.e;
-        //f.Value = rinfo.f;
-        //g.Value = rinfo.g;
-
-        HandleCChangeObj();
-    }
-
-    void HandleCChangeObj()
-    {
-        foreach (var obj in ShowObjs)
-        {
-            if (obj != null && obj.name.Equals(Recieve_B))
+                _id = ClickObjs.FindIndex(x => x == null || x.Equals(null));
+            }
+            else
             {
-                Recieve_ShowItem = obj;
-                break;
+                _id = ClickObjs.IndexOf(ClickedObj);
+            }
+
+            var _iscontained = _id != -1;
+
+            if (!_iscontained) return;
+
+            if (_id >= ShowObjs.Count) return;
+
+            showItem = ShowObjs[_id];
+
+            showItemName = showItem.name;
+
+            if (showItem.gameObject.activeInHierarchy)
+            {
+                WsCChangeInfo wsinfo1 = new WsCChangeInfo()
+                {
+                    a = "hideitem",
+                    b = showItemName,
+                    c = string.Empty,
+                    d = string.Empty,
+                    e = string.Empty,
+                    f = string.Empty,
+                    g = string.Empty,
+                };
+
+                MessageDispatcher.SendMessage(this, WsMessageType.SendCChangeObj.ToString(), wsinfo1, 0);
+            }
+            else
+            {
+                WsCChangeInfo wsinfo1 = new WsCChangeInfo()
+                {
+                    a = "showitem",
+                    b = showItemName,
+                    c = string.Empty,
+                    d = string.Empty,
+                    e = string.Empty,
+                    f = string.Empty,
+                    g = string.Empty,
+                };
+
+                MessageDispatcher.SendMessage(this, WsMessageType.SendCChangeObj.ToString(), wsinfo1, 0);
             }
         }
 
-        if (Recieve_ShowItem == null) return;
 
-        if (Recieve_A.Equals("showitem"))
+        void RecieveCChangeObj(IMessage msg)
         {
-            Recieve_ShowItem.SetActive(true);
-            tween = Recieve_ShowItem.GetComponent<Transform>().DOScale(Vector3.one, 0.5f);
+            WsCChangeInfo rinfo = msg.Data as WsCChangeInfo;
 
-            tween.SetAutoKill(true);
+            Recieve_A = rinfo.a;
+            Recieve_B = rinfo.b;
+            //c.Value = rinfo.c;
+            //d.Value = rinfo.d;
+            //e.Value = rinfo.e;
+            //f.Value = rinfo.f;
+            //g.Value = rinfo.g;
+
+            HandleCChangeObj();
         }
-        else if (Recieve_A.Equals("hideitem"))
+
+        void HandleCChangeObj()
         {
-            tween = Recieve_ShowItem.GetComponent<Transform>().DOScale(Vector3.zero, 0.5f);
+            foreach (var obj in ShowObjs)
+            {
+                if (obj != null && obj.name.Equals(Recieve_B))
+                {
+                    Recieve_ShowItem = obj;
+                    break;
+                }
+            }
 
-            tween.SetAutoKill(true);
+            if (Recieve_ShowItem == null) return;
 
-            tween.OnComplete(() => { Recieve_ShowItem.SetActive(false); });
+            if (Recieve_A.Equals("showitem"))
+            {
+                Recieve_ShowItem.SetActive(true);
+                tween = Recieve_ShowItem.GetComponent<Transform>().DOScale(Vector3.one, 0.5f);
+
+                tween.SetAutoKill(true);
+            }
+            else if (Recieve_A.Equals("hideitem"))
+            {
+                tween = Recieve_ShowItem.GetComponent<Transform>().DOScale(Vector3.zero, 0.5f);
+
+                tween.SetAutoKill(true);
+
+                tween.OnComplete(() => { Recieve_ShowItem.SetActive(false); });
+            }
         }
     }
 }
